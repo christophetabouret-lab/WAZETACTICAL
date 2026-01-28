a9469e37f83c75c2633d37f19fcc9dedba8ce1a5
christophetabouret-lab <christophetabouret@gmail.com>
2026-01-28 09:38:36 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pec-tactique-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
